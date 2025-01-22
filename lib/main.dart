d95b944c057fb7e177c066b175447b1d151c4701
JagdishSingh01 <jatinpawar354@gmail.com>
2025-01-23 00:20:54 +0530

    Google fonts added

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:quiz/quiz.dart';
 
+
 void main() {
   runApp(const Quiz());
 }
--- lib/start_screen.dart
+++ lib/start_screen.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:quiz/quiz.dart';
-import 'package:quiz/questions_screen.dart';
+
 import 'package:google_fonts/google_fonts.dart';
 
 class StartScreen extends StatelessWidget {
